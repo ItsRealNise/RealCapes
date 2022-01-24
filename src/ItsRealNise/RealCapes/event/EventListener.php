bcfe662274ac82d43f8b944edfbf2b513bfc3435
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-01-25 01:55:21 +0700

    y

--- src/ItsRealNise/RealCapes/event/EventListener.php
+++ src/ItsRealNise/RealCapes/event/EventListener.php
@@ -34,6 +34,6 @@ class EventListener implements Listener{
     
     public function onChangeSkin(PlayerChangeSkinEvent $event){
         $player = $event->getPlayer();
-        $this->skin[$player->getName()] = $player->getSkin();
+        $this->plugin->skin[$player->getName()] = $player->getSkin();
     }
 }
